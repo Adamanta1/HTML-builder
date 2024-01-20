7ec899c662fbd4210c35856e39ba9b4046723401
adamanta1 <elena.brashkina2020@gmail.com>
2024-01-20 23:22:35 +0400

    feat: solve 04 task

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+const folder = path.join(__dirname, 'files')
+const newFolder = path.join(__dirname, 'files-copy')
+
+fs.mkdir(newFolder, { recursive: true }, (err) => { // create a copy folder recursively
+  if (err) throw err;
+});
+
+fs.readdir(folder, (err, files) => { // copy files
+  if (err) throw err;
+  else {
+    files.forEach(file => {
+      fs.copyFile(folder + '/' + file, newFolder + '/' + file, (err) => {
+        if (err) throw err;
+      })
+    });
+  }
+})
